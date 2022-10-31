90a43ff38a799b6fffad896e91b2fb0e7b345b87
nonstdout <csalisbury@rocketmail.com>
2022-10-31 14:55:02 +0000

    added tests for connection to dash api

--- meraki_subnets.py
+++ meraki_subnets.py
@@ -1,4 +1,30 @@
-#/bin/python
+#!/usr/bin/env python3
+
+import os, sys
+import meraki
+import logging
 
 def check():
     return True
+
+def main():
+    check_api_key_set('MERAKI_DASHBOARD_API_KEY')
+    dashboard = connect_to_dashboard_api(meraki)
+    orgs = get_orgs(dashboard)
+
+
+def check_api_key_set(key):
+    logging.info("Retrieving MERAKI_DASHBOARD_API_KEY from env vars")
+    if not os.environ.get(key):
+        sys.exit("API Key not set as environment variable")
+
+def connect_to_dashboard_api(api):
+    api = api.DashboardAPI()
+    return api
+
+def get_orgs(dashboard):
+    orgs = dashboard.organizations.getOrganizations()
+    return orgs
+
+if __name__ == "__main__":
+    main()
--- tests.py
+++ tests.py
@@ -1,4 +1,41 @@
-from meraki_subnets import check, get_api_key
+from meraki_subnets import check, check_api_key_set, connect_to_dashboard_api, get_orgs
+import pytest
+
+# Mock the dashboard api for testing
+class DashboardAPI:
+    def __init__(self):
+        self.organizations = Organizations()
+        self.administered = ""
+        self.appliance = ""
+        self.batch = ""
+        self.camera = ""
+        self.cellularGateway = ""
+        self.devices = ""
+        self.insight = ""
+        self.networks = ""
+        self.sensor = "" 
+        self.sm = "" 
+        self.switch = ""
+        self.wireless = ""
+
+class Meraki:
+    def __init__(self):
+        pass
+    
+    def DashboardAPI(self):
+        api = DashboardAPI()
+        return api
+
+class Organizations:
+    def __init__(self):
+        self.orgs = [{'id': '541362', 'name': 'test_org', }]
+    
+    def getOrganizations(self):
+        return self.orgs
+
+
+mock_dash_object = Meraki()
+
 
 def test():
     assert "hello" == "hello"
@@ -6,7 +43,18 @@ def test():
 def test_meraki_subnet():
     assert check() == True
 
-def empty_api_key_fails():
-    if not get_api_key():
-        thing = 1234
-        assert thing == "raise exception"
+def test_empty_api_key_fails():
+    """not having env var set should raise sys.exit()"""
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        check_api_key_set('123test_Key')
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == "API Key not set as environment variable"
+
+def test_connect_to_dashboard_api(): # Refactor to just test we get back a dashboardAPI with the correct keys
+    """test setting up connection to dashboard api"""
+    assert connect_to_dashboard_api(mock_dash_object).organizations.getOrganizations() == [{'id': '541362', 'name': 'test_org', }]
+
+def test_orgs():
+    dashboard = mock_dash_object.DashboardAPI()
+    assert get_orgs(dashboard) == [{'id': '541362', 'name': 'test_org'}]
+
